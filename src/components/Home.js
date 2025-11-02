3bb106db8e538caedcce7cbaaf37f08c8ecf6a66
mateyab <matey.bsm@gmail.com>
2025-11-01 20:03:25 -0700

    fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Hello, I'm Mateya</h1>
-            <h2>Welcome to My Portfolio</h2>
-            <p>I am a Sofware engneering student at the university of victoria</p>
+            <h1><strong>Mateya Berezowsky</strong></h1>
+            <h3>Welcome to My Portfolio</h3>
+            <p>I am a Software engneering student at the University of Victoria with a specialization in data analysis and artifical intelligence.</p>
         </div>
 
     );
